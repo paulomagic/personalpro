bfc2bdd979e890cb367747256a5879bd0e9ee98f
Paulo Ricardo <paulomagicstories@gmail.com>
2026-03-03 20:32:57 -0300

    chore: increment service worker version to v29

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v28';
+const CACHE_VERSION = 'v29';
 const STATIC_CACHE = `personalpro-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `personalpro-runtime-${CACHE_VERSION}`;
 const API_CACHE = `personalpro-api-${CACHE_VERSION}`;

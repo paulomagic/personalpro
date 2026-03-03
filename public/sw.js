8849475b0c7db2de71e385e867bc6f15b421a1ce
Paulo Ricardo <paulomagicstories@gmail.com>
2026-03-03 12:50:02 -0300

    fix(sw): bump cache to v24 to force admin fix in prod

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v23';
+const CACHE_VERSION = 'v24';
 const STATIC_CACHE = `personalpro-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `personalpro-runtime-${CACHE_VERSION}`;
 const API_CACHE = `personalpro-api-${CACHE_VERSION}`;
